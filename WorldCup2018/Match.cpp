0b14d4ebbf236fd759af98e7573d7eaaf923b449
JamesBrown <wck593462925@outlook.com>
2017-11-13 14:49:15 +0800

    添加项目文件。

---
+++ WorldCup2018/Match.cpp
@@ -0,0 +1,5 @@
+#include "Match.h"
+using namespace std;
+vector<string> WorldCup::match(Team home, Team away) {
+
+}
---
+++ WorldCup2018/Match.h
@@ -0,0 +1,8 @@
+#include "Player.h"
+#include "Team.h"
+#include <vector>
+#include <string>
+#ifndef MATCH_H
+#define MATCH_H
+std::vector<std::string> WorldCup::match(Team home, Team away);
+#endif
